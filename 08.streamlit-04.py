836991204bb044157fd12e5f42c93406e30bccbc
Shinminjin <tlsalswls123@gmail.com>
2025-08-05 13:35:00 +0900

    ktds04 파일 삭제

--- 08.streamlit-04.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-x = st.slider("Select a value", 0, 100, 50)
-st.write(f"You selected: {x}")
--- streamlit.sh
+++
@@ -1,4 +0,0 @@
-pip install streamlit
-pip install openai
-pip install dotenv
-python -m streamlit run 10.chat_interface.py --server.port 8000 --server.address 0.0.0.0
